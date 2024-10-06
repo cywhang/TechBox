42201a3afc7a63517983436cfbcb1916d3da34e9
cywha <cywhang2@naver.com>
2024-10-07 03:37:34 +0900

    commit

--- frontend/src/components/Center.vue
+++ frontend/src/components/Center.vue
@@ -2,7 +2,7 @@
     <section class="py-5">
       <div class="container px-4 px-lg-5 mt-5">
           <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-              <div class="col mb-5" v-for="(item, idx) in state.items <= 8" :key="idx">
+              <div class="col mb-5" v-for="(item, idx) in state.items.slice(0, 8)" :key="idx">
                   <Card :item="item"/>
               </div>
           </div>
